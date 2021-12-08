5e639acd684b70484613631ff644ea00827fdbdd
minuxx <minux8905@gmail.com>
2021-12-08 16:53:21 +0900

    Feat: Add Main page

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,15 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./views/pages/Login";
 import Join from "./views/pages/Join";
-import Layout from "./layout/Layout";
 import "./index.css";
-import Header from "./components/Header";
+import Main from "./views/pages/Main";
 import { checkLogin } from "./utils/storage/storageManager";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={checkLogin() ? <Header /> : <Login />} />
+        <Route path="/" element={checkLogin() ? <Main /> : <Login />} />
         <Route exact path="/account" element={<Join />} />
       </Routes>
     </BrowserRouter>
