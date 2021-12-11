c3ff47454e18ec422e5981425c7afd706b96975d
minuxx <minux8905@gmail.com>
2021-12-11 22:42:02 +0900

    Feat: Add filter layout on Home In client module

--- client/src/views/home/Home.js
+++ client/src/views/home/Home.js
@@ -4,6 +4,7 @@ import Post from "./Post";
 
 function Home() {
   const [posts, setPosts] = useState([]);
+  const [filter, setFilter] = useState("all");
 
   useEffect(() => {
     getPosts();
@@ -25,11 +26,47 @@ function Home() {
     }
   };
 
+  const onFilter = (e) => {
+    setFilter(e.target.id);
+  };
+
   return (
-    <div className="grid grid-cols-3 gap-4">
-      {posts.map((post) => (
-        <Post key={post.idx} post={post} />
-      ))}
+    <div>
+      <div className="inline-flex flex-row items-center rounded-md bg-gray-300">
+        <div
+          className={`${filter == "all" ? "bg-blue-500" : null} w-20 text-center text-white rounded-md p-1.5 font-bold cursor-pointer`}
+          id="all"
+          onClick={onFilter}
+        >
+          ALL
+        </div>
+        <div
+          className={`${filter == "writer" ? "bg-blue-500" : null} w-20 text-center text-white rounded-md p-1.5 font-bold cursor-pointer`}
+          id="writer"
+          onClick={onFilter}
+        >
+          작성자
+        </div>
+        <div
+          className={`${filter == "hashtag" ? "bg-blue-500" : null} w-20 text-center text-white rounded-md p-1.5 font-bold cursor-pointer`}
+          id="hashtag"
+          onClick={onFilter}
+        >
+          해시태그
+        </div>
+        <div
+          className={`${filter == "content" ? "bg-blue-500" : null} w-20 text-center text-white rounded-md p-1.5 font-bold cursor-pointer`}
+          id="content"
+          onClick={onFilter}
+        >
+          내용
+        </div>
+      </div>
+      <div className="grid grid-cols-3 gap-4">
+        {posts.map((post) => (
+          <Post key={post.idx} post={post} />
+        ))}
+      </div>
     </div>
   );
 }
